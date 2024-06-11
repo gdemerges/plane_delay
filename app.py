db7bc91c3769b64aae71aac6a83b732cd761391d
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-06-11 06:19:46 +0200

    update

--- app.py
+++ app.py
@@ -3,17 +3,13 @@ import pandas as pd
 import joblib
 from datetime import datetime
 
-# Charger le modèle entraîné
 model = joblib.load('flight_delay_model.pkl')
 
-# Charger les listes de villes de départ et d'arrivée
 origins = joblib.load('origins.pkl')
 destinations = joblib.load('destinations.pkl')
 
-# Titre de l'application
 st.title("Prédiction de Retard de Vol")
 
-# Sélection des villes de départ et d'arrivée
 origin = st.selectbox("Sélectionnez la ville de départ", origins)
 dest = st.selectbox("Sélectionnez la ville d'arrivée", destinations)
 
@@ -22,7 +18,6 @@ date = st.date_input("Sélectionnez la date du vol", datetime.today())
 month = date.month
 dayofmonth = date.day
 
-# Prédire le retard
 if st.button("Prédire le retard"):
     input_data = pd.DataFrame([[origin, dest, month, dayofmonth]], columns=['ORIGIN_CITY_NAME', 'DEST_CITY_NAME', 'MONTH', 'DAY_OF_MONTH'])
     prediction = model.predict(input_data)[0]
